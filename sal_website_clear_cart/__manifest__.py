e8301baec0ed51c2a30cf6528b177853b167181c
salman <odooapp01@gmail.com>
2022-05-01 23:28:58 +0530

    [ADD] index.html

--- sal_website_clear_cart/__manifest__.py
+++ sal_website_clear_cart/__manifest__.py
@@ -4,10 +4,11 @@
     'version':'1.0.0',
     'description':'Remove all products from cart at one click',
     'summary':'Clear Cart',
+    'license':'',
 
     'depends':['website_sale',],
     'data':['views/website_clear_cart.xml'],
-
+    'images':['static/description/icon.png'],
     'installable': True,
     'auto_install':False,
     'price':0,
